d7a9fb4b534064ce0ca9b8b462043ff411730975
Vincytats <vincenttats@gmail.com>
2024-02-14 02:39:39 -0800

    Commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,15 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+#!/bin/bash
+
+# MySQL credentials
+MYSQL_USER="your_mysql_user"
+MYSQL_PASSWORD="your_mysql_password"
+
+# MySQL users to check
+USERS=("user_0d_1" "user_0d_2")
+
+# Loop through each user and retrieve privileges
+for USER in "${USERS[@]}"; do
+    echo "Privileges for $USER:"
+    mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW GRANTS FOR '$USER'@'localhost';"
+    echo "-------------------------------------"
+done
